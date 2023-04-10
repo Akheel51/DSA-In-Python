e82eeb70376cbb1ce698d8518cd6b09968f2d177
Md Akheel Baba <mohdakheelitb@gmail.com>
2023-04-10 18:17:05 +0530

    Time: 195 ms (29.49%), Space: 26.2 MB (5.92%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        d={}
+        for i in nums:
+            if i in d:
+                d[i]+=1
+            else:
+                d[i]=1
+        for i in d:
+            if d[i]==1:
+                return i
+        
