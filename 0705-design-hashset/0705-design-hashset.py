f237512b36145180fb891700c29f5741e34b4ace
Md Akheel Baba <mohdakheelitb@gmail.com>
2023-07-18 19:11:08 +0530

    Time: 151 ms (86.96%), Space: 22.2 MB (53.92%) - LeetHub

---
+++ 0705-design-hashset/0705-design-hashset.py
@@ -0,0 +1,16 @@
+class MyHashSet:
+
+    def __init__(self):
+        self.mp=defaultdict(int)
+        
+
+    def add(self, key: int) -> None:
+        self.mp[key]=True
+        
+
+    def remove(self, key: int) -> None:
+        self.mp[key]=False
+
+    def contains(self, key: int) -> bool:
+        return self.mp[key]
+        
