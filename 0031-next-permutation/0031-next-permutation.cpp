db559d8bc8b72cf7a659b485693ca3631a665450
Md Akheel Baba <mohdakheelitb@gmail.com>
2023-07-21 11:52:34 +0530

    Time: 3 ms (82.46%), Space: 12.1 MB (60.27%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>&A) {
+        int idx=-1;
+        int n= A.size();
+        for(int i=n-2;i>=0;i--){
+            if(A[i]<A[i+1]){
+                idx=i;
+                break;
+            }
+        }
+        if(idx==-1){
+            reverse(A.begin(),A.end());
+        }
+        else{
+        for(int i=n-1;i>idx;i--){
+            if(A[i]>A[idx]){
+                swap(A[i],A[idx]);
+                break;
+            }
+        }
+        reverse(A.begin()+idx+1,A.end());
+        }
+        
+    }
+};
